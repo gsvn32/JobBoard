0a954c55306f1407ce9b84ee97aa8d5440f6daf2
Nikhil <40881492+gsvn32@users.noreply.github.com>
2024-03-30 19:12:45 -0500

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JobsBoard.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
